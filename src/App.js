6cc37e0b28a5d014e0701bc02d88206bb42a53c4
Ctucker9233 <court.m.tucker@gmail.com>
2020-02-14 19:16:58 -0800

    Added main body framework

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/index";
+import Main from "./components/Main/index"
 
 function App() {
   return (
     <Router>
       <div>
         <Navigation />
+        <Main />
         {/* Based on `this.state.currentPage`, render the appropriate component
           here. */}
 
---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const styles = {
+    h1: {
+        textAlign: "center"
+    }
+}
+
+function Main(){
+    return(
+        <h1 style={styles.h1}>Employee Directory</h1>
+    )
+}
+
+export default Main;
