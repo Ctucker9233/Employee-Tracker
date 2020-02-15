63c5e07b7fbd0a1c4ebd123400776e376766e8b0
Ctucker9233 <court.m.tucker@gmail.com>
2020-02-14 18:41:12 -0800

    Added navbar framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Navigation />
+        {/* Based on `this.state.currentPage`, render the appropriate component
+          here. */}
+
+      </div>
+    </Router>
+  )
 }
 
 export default App;
